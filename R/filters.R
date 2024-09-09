f2f6c5f1e39f1bb55c87231b927a65bc951fc16b
Rajan Shankar <rsha5949@uni.sydney.edu.au>
2024-09-09 22:07:00 +1000

    added compute_objective

--- R/filters.R
+++ R/filters.R
@@ -38,3 +38,27 @@ huber_robust_filter = function(y, par, build) {
   return(out)
 }
 
+#' A Cat Function
+#'
+#' This function allows you to express your love of cats.
+#'
+#' @importFrom magrittr %>%
+#' @importFrom foreach %dopar%
+#' @param love Do you love cats? Defaults to TRUE.
+#' @details
+#' Additional details...
+#' @returns description
+#' @export
+compute_objective = function(y, par, build, gamma = array(0, dim(y))) {
+
+  out = fn_filter(y = y,
+                  par = par,
+                  build = build,
+                  gamma = gamma,
+                  return_obj = TRUE)
+  return(out)
+}
+
+
+
+
